1050684c051d6b46e5c3bdad59c6c3650fa248fb
AlinurMammadzada5 <alinurmammadzada@gmail.com>
2026-05-06 19:03:17 +0200

    add responsiveness

--- portfolio-v1/src/App.tsx
+++ portfolio-v1/src/App.tsx
@@ -1,7 +1,7 @@
-import Contact from "./Contact"
 import Navbar from "./Navbar";
 import Hero from "./Hero"
 import AboutMe from "./AboutMe";
+import Contact from "./Contact"
 import Footer from "./Footer";
 import Experience from "./Experience";
 function App() {
