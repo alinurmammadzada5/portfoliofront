e3c856e83ee37645fa18d2fc8e721f6d3c42180f
AlinurMammadzada5 <alinurmammadzada@gmail.com>
2026-05-08 17:32:53 +0200

    add about me

--- portfolio-v1/src/App.tsx
+++ portfolio-v1/src/App.tsx
@@ -1,15 +1,15 @@
 import Navbar from "./Navbar";
 import Hero from "./Hero"
+import AboutMe from "./AboutMe";
 // import Footer from "./Footer";
-// import AboutMe from "./AboutMe";
 // import Contact from "./Contact"
 // import Experience from "./Experience";
 function App() {
   return <div>
     <Navbar />
     <Hero />
-    {/* <AboutMe />
-    <Experience />
+    <AboutMe />
+    {/* <Experience />
     <Contact /> */}
     {/* <Footer /> */}
     </div>
--- portfolio-v1/src/main.tsx
+++ portfolio-v1/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './css/navbar.css'
 import './css/hero.css'
+import './css/aboutme.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
